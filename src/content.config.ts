c2507c015afab67af17ff0a59be8c9980981cc2e
Alma <xayozijopi960@gmail.com>
2026-07-29 06:25:46 +0800

    移除文档库

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,68 +2,6 @@ import { defineCollection } from 'astro:content';
 import { z } from 'astro/zod';
 import { glob } from 'astro/loaders';
 
-const metadataDefinition = () =>
-  z
-    .object({
-      title: z.string().optional(),
-      ignoreTitleTemplate: z.boolean().optional(),
-
-      canonical: z.url().optional(),
-
-      robots: z
-        .object({
-          index: z.boolean().optional(),
-          follow: z.boolean().optional(),
-        })
-        .optional(),
-
-      description: z.string().optional(),
-
-      openGraph: z
-        .object({
-          url: z.string().optional(),
-          siteName: z.string().optional(),
-          images: z
-            .array(
-              z.object({
-                url: z.string(),
-                width: z.number().optional(),
-                height: z.number().optional(),
-              })
-            )
-            .optional(),
-          locale: z.string().optional(),
-          type: z.string().optional(),
-        })
-        .optional(),
-
-      twitter: z
-        .object({
-          handle: z.string().optional(),
-          site: z.string().optional(),
-          cardType: z.string().optional(),
-        })
-        .optional(),
-    })
-    .optional();
-
-const docsCollection = defineCollection({
-  loader: glob({ pattern: ['*.md', '*.mdx'], base: 'src/content/docs' }),
-  schema: z.object({
-    publishDate: z.date().optional(),
-    updateDate: z.date().optional(),
-    draft: z.boolean().optional().default(false),
-
-    title: z.string(),
-    description: z.string().optional(),
-
-    category: z.string(),
-    tags: z.array(z.string()).optional().default([]),
-
-    metadata: metadataDefinition(),
-  }),
-});
-
 const postsCollection = defineCollection({
   loader: glob({ pattern: ['*.md', '*.mdx'], base: 'src/content/posts' }),
   schema: z.object({
@@ -87,5 +25,4 @@ const postsCollection = defineCollection({
 
 export const collections = {
   posts: postsCollection,
-  docs: docsCollection,
 };
