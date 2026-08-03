c2e8c1ad4da27e5ccfa18ea687c69fe219e18b2c
Alma <xayozijopi960@gmail.com>
2026-08-03 20:36:51 +0800

    修复

--- packages/rich-text-editor/src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as EditorMount } from './core/EditorMount';
-export type { EditorMountProps } from './core/EditorMount';
--- packages/rich-text-editor/src/engine/registry/index.ts
+++
@@ -1,4 +0,0 @@
-export type { MdxComponentDefinition } from './types';
-export { registerComponent, getComponent, getRegisteredNames, isRegistered, clearRegistry } from './components';
-export { calloutPropsSchema, figurePropsSchema } from './schemas';
-export type { CalloutProps, FigureProps } from './schemas';
