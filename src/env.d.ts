f3c6d585104782ecacb688e4e05d03d35013e4d9
Alma <xayozijopi960@gmail.com>
2026-08-04 07:54:47 +0800

    1

--- src/env.d.ts
+++ src/env.d.ts
@@ -44,3 +44,7 @@ declare module 'virtual:config-community' {
   const config: Record<string, unknown>;
   export default config;
 }
+
+declare module '~/scripts/blog-init.js';
+declare module '~/scripts/blog-transitions.js';
+declare module '~/scripts/blog-photoswipe.js';
